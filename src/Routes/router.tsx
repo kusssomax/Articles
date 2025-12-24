52a6ec12d5ec87dfdf4bbd768b0fd3263183357d
kusssomax <maxkusjo@gmail.com>
2025-12-24 22:48:31 +0200

    page routes

--- src/Routes/router.tsx
+++ src/Routes/router.tsx
@@ -1,12 +1,17 @@
+import MainLayout from "@/layouts/MainLayout/MainLayout";
+import HomePage from "@/pages/HomePage/HomePage";
 import { Routes, Route } from "react-router-dom";
+import ArticlePage from "@/pages/ArticlePage/ArticlePage";
 
 
 export const AppRoutes = () => {
 
     return (
         <Routes>
-            <Route path="/" element={<div>Home</div>} />
-            <Route path="/article" element={<div>Article</div>} />
+            <Route path="/" element={<MainLayout />}>
+                <Route index element={<HomePage />} />
+            </Route>
+                <Route path="/article/:id" element={<ArticlePage />} />
         </Routes>
     )
 }
